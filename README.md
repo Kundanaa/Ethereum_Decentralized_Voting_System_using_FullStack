5d46297207631c031968abe3659d84bcc33f971f
Kundanaa <112239353+Kundanaa@users.noreply.github.com>
2024-02-11 16:03:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# votesystem
+ Context:Electronic Voting in India , The strong technical safeguards in electronic voting machines and elaborate administrative safeguards, procedures and security put in place by ECI ensure that the election is transparent, free and fair.
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2024 <a href="https://www.linkedin.com/in/kundanaa-pilla-36629124b/">Kundanaa Pilla</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
